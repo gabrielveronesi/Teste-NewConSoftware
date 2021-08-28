7e110973bd5494d210d2faf186e0d2cf6867d732
Gabriel Veronesi <57780382+gabrielveronesi@users.noreply.github.com>
2021-08-28 18:10:40 -0300

    Colocando outro projeto com sln

--- Startup.cs
+++ Startup.cs
@@ -1,23 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Back_End.Data;
-using Back_End.Data.Interfaces;
-using Back_End.Data.Repository;
-using Back_End.Validator;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Back_End
+namespace WebApplication1
 {
     public class Startup
     {
@@ -28,38 +23,25 @@ namespace Back_End
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //Configurando swagger
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Back_End", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication1", Version = "v1" });
             });
-
-            //Configurando o banco de dados
-            services.AddDbContext<DataContext>(
-            context => context.UseSqlServer(Configuration.GetConnectionString("MySqlConnection")
-            ));
-
-            //Configurando injeção de dependencia
-            services.AddTransient<IPontoTuristico, PontoTuristicoRepository>();
-            services.AddTransient<PontoTuristicoValidator, PontoTuristicoValidator>();
-
-            
-
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //Configurando cors
-            app.UseCors( x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Back_End v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication1 v1"));
             }
 
             app.UseHttpsRedirection();
